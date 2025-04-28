3c7842e6f348652a0c6a04d0d732badb9d13731a
Dani <danieltefera14@gmail.com>
2025-04-28 10:28:26 +0300

    portfolio profile updated

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {
+  NzNotificationService,
+  NzNotificationPlacement,
+} from 'ng-zorro-antd/notification';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
-
+  constructor(private notification: NzNotificationService) {}
+  onSend(): void {
+    this.notification.blank(
+      'Notification Title',
+      'This service is not available righ now, please use the social media links to conatct me.'
+    );
+  }
 }
